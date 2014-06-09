19b055f3c141b0d37755a23cfddc3b52e511c548
Snir <snir@gizra.com>
2014-06-09 10:02:56 +0300

    Security layer service

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,14 +1,21 @@
 'use strict';
 
 angular.module('myApp')
-  .controller('LoginCtrl', function ($scope, Security) {
+  .controller('LoginCtrl', ['$scope', '$cookies', 'Security', function($scope, $cookies, Security) {
     $scope.user = {};
 
 
     $scope.login = function() {
-      Security.login(user, function(res) {
+      Security.login($scope.user, function(res) {
+        // Success.
+
         $location.path('/dashboard');
+      },
+      function(res) {
+        // Error.
+
+        console.log('Error in login');
       });
     }
 
-  });
+  }]);
--- app/scripts/services/user/security.js
+++ app/scripts/services/user/security.js
@@ -4,21 +4,35 @@ angular.module('myApp').
   service('Security', function($http, User) {
 
     this.status = function(success, error) {
-      $http.get('/status').success(function(res) {
-        User.setDetails(res);
-        success();
+      $http.get('http://127.0.0.1/login.php?status=1').success(function(res) {
+        if (res.status !== "undefined" && res.token) {
+          User.setDetails(res);
+          success();
+        }
       }).error(error);
     }
 
     this.login = function(user, success, error) {
-      $http.post('/login').success(function(res) {
-        User.setUserDetails(res);
-        success();
+      $http.post('http://127.0.0.1/login.php', user,
+        {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+          transformRequest: function(data){
+            return $.param(data);
+          }
+        }
+      ).success(function(res) {
+        if (res.status !== "undefined" && res.token) {
+          User.setUserDetails(res);
+          success();
+        }
+        else {
+          error();
+        }
       }).error(error);
     }
 
     this.logout = function(success, error) {
-      $http.post('/logout').success(function(res) {
+      $http.post('http://127.0.0.1/login.php?logout=1').success(function(res) {
         User.clear();
         success();
       }).error(error);
